6f302bc12844d51b35cb162d4e701fd5d1f2595b
Brian Hernandez <bhernand@us.ibm.com>
2016-11-11 15:36:36 -0600

    test

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "PostgreSQL",
 	providers: [
-		.Apt("pq")
+		.Apt("libpq-dev")
 	],	
 	dependencies: [
 		.Package(url: "https://github.com/Zewo/CLibpq.git", majorVersion: 0, minor: 13),
